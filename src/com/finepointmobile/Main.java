bf34b4aa910a7dd218c15568c8e2dd48d32836db
Daniel Malone <danieljmalone@gmail.com>
2017-01-15 14:11:29 -0600

    finish challenge

--- src/com/finepointmobile/Main.java
+++ src/com/finepointmobile/Main.java
@@ -1,8 +1,30 @@
 package com.finepointmobile;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        String sentence = "A slow yellow fox crawls under the proactive dog.";
+        ArrayList<Character> hasCharacters = new ArrayList<Character>();
+
+        // convert to lower case?
+        sentence = sentence.toLowerCase();
+
+        for (int i = 0; i < alphabet.length; i++) {
+            if (!sentence.contains(String.valueOf(alphabet[i]))) {
+                hasCharacters.add(alphabet[i]);
+            }
+        }
+
+        if (hasCharacters.size() == 0) {
+            System.out.println("NULL");
+        } else {
+            for (int i = 0; i < hasCharacters.size(); i++) {
+                System.out.print(hasCharacters.get(i));
+            }
+        }
+
     }
 }
