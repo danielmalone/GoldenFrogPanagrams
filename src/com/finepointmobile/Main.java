572a79a874e0af3b6ec3e2b0b443bb0fb63cce41
Daniel Malone <danieljmalone@gmail.com>
2017-01-15 18:56:48 -0600

    rename variable

--- src/com/finepointmobile/Main.java
+++ src/com/finepointmobile/Main.java
@@ -7,22 +7,22 @@ public class Main {
     public static void main(String[] args) {
         char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         String sentence = "A slow yellow fox crawls under the proactive dog.";
-        ArrayList<Character> hasCharacters = new ArrayList<Character>();
+        ArrayList<Character> characters = new ArrayList<Character>();
 
         // convert to lower case?
         sentence = sentence.toLowerCase();
 
         for (int i = 0; i < alphabet.length; i++) {
             if (!sentence.contains(String.valueOf(alphabet[i]))) {
-                hasCharacters.add(alphabet[i]);
+                characters.add(alphabet[i]);
             }
         }
 
-        if (hasCharacters.size() == 0) {
+        if (characters.size() == 0) {
             System.out.println("NULL");
         } else {
-            for (int i = 0; i < hasCharacters.size(); i++) {
-                System.out.print(hasCharacters.get(i));
+            for (int i = 0; i < characters.size(); i++) {
+                System.out.print(characters.get(i));
             }
         }
 
